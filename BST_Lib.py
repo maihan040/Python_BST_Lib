810f319161eb72353e17e454b560e91eae140fe7
codex040 <codexhh040@gmail.com>
2021-02-12 07:51:23 -0800

    Created new module "BST_Lib.py"

---
+++ BST_Lib.py
@@ -0,0 +1,58 @@
+'''
+	BST_Lib.py
+
+	Module to define the main functions that can be carried out on a binary search tree
+
+	Created: 02/12/2021
+
+	Verison: 1.0
+
+	Requires: "BST_Node.py" module
+'''
+
+#################################### Imports ############################################
+#                                                                                       #
+# BST_Node.py: This will be the main module needed in order to build the binary search	#
+# 			  tree.							                                            #
+#########################################################################################
+import BST_Node
+
+################################### Functions ###########################################
+#                                                                                       #
+# BST_Node.py: This will be the main module needed in order to build the binary search	#
+# 			  tree.							                                            #
+#																						#
+# Following functions are defined below:												#
+#########################################################################################
+
+
+
+############################### Append Function #########################################
+#                                                                                       #
+# Append function. This will add a new element to the tree in the corresponding order	#
+# 																						#
+# Binary search trees have the characteristic of the left child always being less than	#
+# or equal to the current node (the parent) value, and the right child being greater	# 
+# than the parent															 			#
+# 						                                            					#
+# Input parameters: "root" which is the root of the current binary search tree where	#
+# 				   user wishes to add a new node										#
+# 				   "val" the value of the new node that will need to be added to the 	#
+# 			       tree.																#
+# 																						#
+# Output: Nothing																		#
+#########################################################################################
+def append_bst(root, val): 
+
+	#validation check 
+	if root == None: 
+		return BST_Node.Node(val) 
+
+	#recursively check which leaf will be the suited parent for this value 
+	if root.val <= val: 
+		root.right = append_bst(root.right, val)
+	else: #traverse the left side 
+		root.left = append_bst(root.left, val)
+
+	#return the root 
+	return root 
--- BST_Node.py
+++ BST_Node.py
@@ -1,21 +1,21 @@
 '''
 	BST_Node.py
 
-	module to define the BST node class which will be used by the subsequent BST_Lib module
+	Module to define the BST node class which will be used by the subsequent BST_Lib module
 
-	created: 02/11/2021
+	Created: 02/11/2021
 
-	verison: 1.0
+	Verison: 1.0
 '''
 
 ################################## Class Definition #####################################
 #                                                                                       #
 #Notes: Initially, nodes will have no children which is why the "left" and "right"      #
-#       attributes are referring to "None"                                               #
+#       attributes are referring to "None"                                              #
 #                                                                                       #
-#       Should the user not supply a data value for the "vaL" attribute a "NaN"         #
+#       Should the user not supply a data value for the "vaL" attribute, a "NaN"        #
 #       (not a number) will be added by default to indicate that this                   #
-#       node doesn't contain a usefull value                                            #
+#       node doesn't contain a useful value                                             #
 #########################################################################################
 
 class BST_Node:
