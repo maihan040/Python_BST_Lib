2179e0e72b8c8675ef5e48942da7f5c2a242c79f
codex040 <codexhh040@gmail.com>
2021-02-13 20:42:17 -0800

    Changed tab space to 8

--- BST_Lib.py
+++ BST_Lib.py
@@ -11,35 +11,35 @@
 '''
 
 ######################################### Imports ###############################################
-#																								#
+#												#
 # BST_Node.py: This will be the main module needed in order to build the binary search tree.	#
-# 																								#
+#												#
 #################################################################################################
 import BST_Node
 
 ####################################### Functions ###############################################
-#																								#
+#												#
 # BST_Node.py: This will be the main module needed in order to build the binary search tree.	#
-#																								#
-#																								#
-# Following functions are defined below:														#
-#																								#
+#												#
+#												#
+# Following functions are defined below:							#
+#												#
 #################################################################################################
 
 ##################################### Append Function ###########################################
-#																								#
-# Append function. This will add a new element to the tree in the corresponding order			#
-# 																								#
-# Binary search trees have the characteristic of the left child always being less than			#
-# or equal to the current node (the parent) value, and the right child being greater			#
-# than the parent																				#
-#																								#	
-# Input parameters: "root" which is the root of the current binary search tree where user		#
-# wishes to add a new node "val" 																#
-# 		"val" the value of the new node that will need to be added to the tree tree.			#
-# 																								#
-# Output: None																					#
-#																								#
+#												#
+# Append function. This will add a new element to the tree in the corresponding order		#
+# 												#
+# Binary search trees have the characteristic of the left child always being less than		#
+# or equal to the current node (the parent) value, and the right child being greater		#
+# than the parent										#
+#												#	
+# Input parameters: "root" which is the root of the current binary search tree where user	#
+# wishes to add a new node "val" 								#
+# 		"val" the value of the new node that will need to be added to the tree tree.	#
+# 												#
+# Output: None											#
+#												#
 #################################################################################################
 def append_bst(root, val): 
 
